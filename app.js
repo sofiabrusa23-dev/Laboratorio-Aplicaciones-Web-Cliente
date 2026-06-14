940a29ed93407d91c1abb50a44c7e00a62428463
sofia brusa <sofiabrusa23@gmail.com>
2026-06-13 23:28:46 -0300

    Modal para el detalle de los producto y modificacion del carrusel

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const productsdiv = document.getElementById('products');
+let productos = [];
 
 // Función para inicializar la web
 async function inicializar() {
     const products = await traerProductos();
+    productos = products; 
     renderizarProductos(products);
+    activarBotonesVerMas();
 
 }
 
@@ -37,12 +40,65 @@ function crearCard(product) {
 
         <div class="product-card_footer">
           <p class="product-card_price">$${product.price}</p>
-          <button class="product-card_button">Ver más</button>
+          <button class="product-card_button" data-id="${product.id}">Ver más</button>
         </div>
       </div>
     </article>
   `;
 }
+
+
+// Funcion mapear los botones para abrir el modal
+function activarBotonesVerMas() {
+  const botonesVerMas = document.querySelectorAll(".product-card_button");
+
+  botonesVerMas.forEach(boton => {
+    boton.addEventListener("click", () => {
+      const idProducto = Number(boton.dataset.id);
+      const productoSeleccionado = productos.find(product => product.id === idProducto);
+
+      abrirModal(productoSeleccionado);
+    });
+  });
+}
+
+
+
+
+// funcion para abrir modal 
+function abrirModal(product) {
+  const modal = document.getElementById("modalProducto");
+
+  const modalImagen = document.getElementById("modalImagen");
+  const modalTitulo = document.getElementById("modalTitulo");
+  const modalCategoria = document.getElementById("modalCategoria");
+  const modalDescripcion = document.getElementById("modalDescripcion");
+  const modalPrecio = document.getElementById("modalPrecio");
+
+  modalImagen.src = product.image;
+  modalImagen.alt = product.title;
+  modalTitulo.textContent = product.title;
+  modalCategoria.textContent = product.category;
+  modalDescripcion.textContent = product.description;
+  modalPrecio.textContent = `$${product.price}`;
+
+  modal.classList.add("modal-producto_activo");
+}
+
+function cerrarModal() {
+  const modal = document.getElementById("modalProducto");
+  modal.classList.remove("modal-producto_activo");
+}
+
+document.getElementById("cerrarModal").addEventListener("click", cerrarModal);
+
+document.getElementById("modalProducto").addEventListener("click", (event) => {
+  if (event.target.id === "modalProducto") {
+    cerrarModal();
+  }
+});
+
+
 
 inicializar();
 
